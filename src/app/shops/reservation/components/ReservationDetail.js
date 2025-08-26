97affe235d5a6c061fd75477fa12bdd972c132a3
JUHYE0925 <rhkdlf48301@gmail.com>
2025-08-27 01:27:59 +0900

    delete http://localhost:8080 url

--- src/app/shops/reservation/components/ReservationDetail.js
+++ src/app/shops/reservation/components/ReservationDetail.js
@@ -18,7 +18,6 @@ export default function ReservationDetail ({resvCode, onClose}) {
 
             try {
                 const res = await fetch(
-                    // `http://localhost:8080/api/v1/shops/reservation/${resvCode}`, {
                     `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/shops/reservation/${resvCode}`, {
                         method: 'GET',
                         headers: {
@@ -48,7 +47,6 @@ export default function ReservationDetail ({resvCode, onClose}) {
 
         try {
             const res = await fetch(
-                // `http://localhost:8080/api/v1/shops/reservation/${resvCode}`,
                 `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/shops/reservation/${resvCode}`,
                 {
                     method: 'DELETE',
